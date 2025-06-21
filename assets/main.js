834e22dd201d44e6b5b1b2fb7d78c67733263e7c
FvCastr0 <fvdc2008@gmail.com>
2025-06-21 08:59:34 -0300

    Sending emails

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+function handleSubmitForm(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const contact = document.getElementById("contact").value;
+  const service = document.getElementById("service").value;
+
+  if (service === "") {
+    alert("Por favor, selecione um serviço.");
+    return;
+  }
+
+  emailjs.init("pTP6zWYYpK4UjRN7G");
+
+  const templateParams = {
+    name: name,
+    email: email,
+    contact: contact,
+    service: service
+  };
+
+  emailjs.send("service_kexy56d", "template_igmbkf1", templateParams).then(
+    () => {
+      alert("E-mail enviado com sucesso!");
+    },
+    error => {
+      console.error("Erro ao enviar o e-mail:", error);
+      alert("Erro ao enviar o e-mail. Tente novamente.");
+    }
+  );
+}
